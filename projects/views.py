cc0d866bfb6eca1d8d6d65fd5b34c532830d03bb
BesnikShala <bes.shala@gmail.com>
2022-06-29 20:06:18 +0000

    Add projects.html

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Project
 
-# Create your views here.
+
+def all_projects(request):
+    """A view to see all products"""
+
+    projects = Project.objects.all()
+
+    context = {
+        'projects': projects,
+    }
+
+    return render(request, 'projects/projects.html', context)
