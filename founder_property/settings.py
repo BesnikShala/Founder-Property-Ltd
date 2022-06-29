7872e6d57ee4ae8279bd326ab3e078a94a986c85
BesnikShala <bes.shala@gmail.com>
2022-06-29 13:20:47 +0000

    Added base.html

--- founder_property/settings.py
+++ founder_property/settings.py
@@ -80,6 +80,7 @@ DATABASES = {
     }
 }
 
+SITE_ID = 1
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -116,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+CSRF_TRUSTED_ORIGINS = ['https://*.example.com', 'https://*.gitpod.io']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
