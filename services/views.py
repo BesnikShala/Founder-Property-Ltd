6f4a04d96a4392497e3bd310cdd7149c394ecb9b
BesnikShala <bes.shala@gmail.com>
2022-06-30 13:48:44 +0000

    Add services urls and json data

--- services/views.py
+++ services/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def services(request):
+    """ View to return services page """
+    return render(request, 'services/services.html')
