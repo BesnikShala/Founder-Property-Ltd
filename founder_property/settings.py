482c36643ed1547649ad384c7c1f0c580ac810e4
BesnikShala <bes.shala@gmail.com>
2022-08-16 19:44:39 +0000

    Deployment to Heroku

--- founder_property/settings.py
+++ founder_property/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 import os
+import dj_database_url
 
 if os.path.exists('env.py'):
     import env  # noqa
@@ -93,12 +94,17 @@ WSGI_APPLICATION = 'founder_property.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 SITE_ID = 1
 
